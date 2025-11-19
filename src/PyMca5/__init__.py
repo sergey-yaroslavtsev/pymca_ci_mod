3edc6860759555fb58bd6435c4d921a8522c9556
Sergey Yaroslavtsev <sergey.yaroslavtsev@esrf.fr>
2025-11-19 19:25:13 +0100

    comment secrets, try trigger

--- src/PyMca5/__init__.py
+++ src/PyMca5/__init__.py
@@ -27,7 +27,7 @@ __author__ = "V.A. Sole"
 __contact__ = "sole@esrf.fr"
 __license__ = "MIT"
 __copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
-__version__ = "5.9.5rc2"
+__version__ = "5.9.5rc3"
 
 import os
 import sys
