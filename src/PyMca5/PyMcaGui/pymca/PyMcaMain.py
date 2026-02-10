e562418bd450b8c7f7ab30fdeedf14c1c5de5459
Sergey Yaroslavtsev <sergey.yaroslavtsev@esrf.fr>
2026-02-10 21:39:12 +0100

    restore original form for test MacOS without signiture

--- src/PyMca5/PyMcaGui/pymca/PyMcaMain.py
+++ src/PyMca5/PyMcaGui/pymca/PyMcaMain.py
@@ -32,8 +32,6 @@ import os
 import sys, getopt
 import traceback
 import logging
-import multiprocessing
-
 if sys.platform == 'win32':
     import ctypes
     from ctypes.wintypes import MAX_PATH
@@ -179,13 +177,6 @@ from PyMca5 import PyMcaDataDir
 __version__ = PyMca5.version()
 
 if __name__ == "__main__":
-
-    multiprocessing.freeze_support()
-
-    # On macOS, set the executable for child processes to prevent -B flag issues
-    if getattr(sys, 'frozen', False):
-        print("Running PyMca in a frozen environment, setting multiprocessing executable")
-        multiprocessing.set_executable(sys.executable)
 
     sys.excepthook = qt.exceptionHandler
 
